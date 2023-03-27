843217783e7be673292e4c72e4dbffa021c56dc1
howie1329 <46204910+howie1329@users.noreply.github.com>
2023-03-26 20:20:33 -0400

    Initial Commit

---
+++ basketballGM/basketballGMApp.swift
@@ -0,0 +1,17 @@
+//
+//  basketballGMApp.swift
+//  basketballGM
+//
+//  Created by Howard Thomas on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct basketballGMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
